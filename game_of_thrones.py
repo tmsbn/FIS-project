b0a3eb4eb3597c57c4eb0dfa9cc16cb81cc88882
Thomas Binu <mailthomasb@gmail.com>
2017-04-16 15:47:09 -0400

    Pandas integration

--- game_of_thrones.py
+++ game_of_thrones.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import tree
@@ -7,8 +8,16 @@ import numpy as np
 
 def main():
     data_frame = pd.read_csv('test_data_set.csv')
+    print(data_frame)
+
+    # dummies
     df_with_dummies = pd.get_dummies(data_frame, columns=['house'])
-    print(df_with_dummies)
+
+    clf = tree.DecisionTreeClassifier()
+    target_column = df_with_dummies['is_alive'].values
+    df_data = df_with_dummies.drop('is_alive', axis=1)
+    print(df_data.loc[:, :].values)
+
 
 if __name__ == '__main__':
     main()
