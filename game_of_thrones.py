02bec86e5c980f98b4e48db0617d1a5c4df0ea91
Thomas Binu <mailthomasb@gmail.com>
2017-04-16 15:06:15 -0400

    testing

--- game_of_thrones.py
+++ game_of_thrones.py
@@ -1,2 +1,14 @@
-import math
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+import numpy as np
 
+
+def main():
+    data_frame = pd.read_csv('test_data_set.csv')
+    df_with_dummies = pd.get_dummies(data_frame, columns=['house'])
+    print(df_with_dummies)
+
+if __name__ == '__main__':
+    main()
